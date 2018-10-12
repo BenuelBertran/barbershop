2886c683625b48f83af95bd1b46f9f5cbdc8df75
Алиса <Alice@test.ru>
2018-10-12 16:02:32 +0300

    Обновил весь Barbershop по BEM

--- js/modal-user.js
+++ js/modal-user.js
@@ -1,14 +1,17 @@
-var user_link = document.querySelector(".user-link");
-var modal_user = document.querySelector(".modal-user");
-var modal_form = modal_user.querySelector(".modal-form");
-var login_field = modal_user.querySelector("[type=text]");
-var password_field = modal_user.querySelector("[type=password]");
+var user_link = document.querySelector(".js-user__link");
+var modal_user = document.querySelector(".js-modal-user");
+var modal_form = modal_user.querySelector(".js-modal-user__form");
+var login_field = modal_user.querySelector(".js-modal-user__login");
+var password_field = modal_user.querySelector(".js-modal-user__password");
 var login_storage = localStorage.getItem("login_field");
-var user_close = modal_user.querySelector(".modal-close");
+var user_close = modal_user.querySelector(".js-modal__close");
 
 user_link.addEventListener("click", function(evt) {
     evt.preventDefault();
-    modal_user.classList.add("modal-show");
+    if (modal_user.classList.contains("js-modal-invalid")) {
+        modal_user.classList.remove("js-modal-invalid");
+    }
+    modal_user.classList.add("js-modal-show");
     if (login_storage) {
         password_field.focus();
     } else {
@@ -19,7 +22,7 @@ user_link.addEventListener("click", function(evt) {
 modal_form.addEventListener("submit", function(evt) {
     if (!login_field.value || !password_field.value) {
         evt.preventDefault();
-        modal_user.classList.add("modal-invalid");
+        modal_user.classList.add("js-modal-invalid");
     } else {
         localStorage.setItem("login_field", login_storage.value);
     }
@@ -27,13 +30,13 @@ modal_form.addEventListener("submit", function(evt) {
 
 user_close.addEventListener("click", function(evt) {
     evt.preventDefault();
-    modal_user.classList.remove("modal-show");
+    modal_user.classList.remove("js-modal-show");
 });
 
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
-        if (modal_user.classList.contains("modal-show")) {
-            modal_user.classList.remove("modal-show");
+        if (modal_user.classList.contains("js-modal-show")) {
+            modal_user.classList.remove("js-modal-show");
         }
     }
 });
